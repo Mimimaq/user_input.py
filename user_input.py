127c2d1213cbfff796270ace79046822567bf839
Mimimaq <emmaq8@gmail.com>
2024-03-04 18:22:41 +0300

    added user_input.py file

---
+++ user_input.py
@@ -0,0 +1,11 @@
+msg = ("What is your name?")
+print(msg)
+
+msg = ("What is your age?")
+print(msg)
+
+msg = ("What is your location?")
+print(msg)
+
+msg = ("Hello Mary. You're 10 years old and live in Nairobi, Kenya.")
+print(msg)
